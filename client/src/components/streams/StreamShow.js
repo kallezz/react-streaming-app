f04840a06137d8a3d86dd2a7dbb6bfcc9b01cfd8
Kalle Melender <kalle.melender@hotmail.com>
2020-10-15 21:10:17 +0300

    rtmp + flv player

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,12 +1,45 @@
 import React, {Component} from 'react';
+import flv from 'flv.js';
 import {connect} from "react-redux";
 import {fetchStream} from "../../actions";
 import {Grid, Header, Icon, Loader, Segment} from "semantic-ui-react";
 
 class StreamShow extends Component {
-  componentDidMount() {
-    this.props.fetchStream(this.props.match.params.id)
+  constructor(props) {
+    super(props);
+
+    this.videoRef = React.createRef();
   }
+
+  componentDidMount() {
+    const {id} = this.props.match.params;
+
+    this.props.fetchStream(id);
+    this.buildPlayer();
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    this.buildPlayer();
+  }
+
+  componentWillUnmount() {
+    this.player.destroy();
+  }
+
+  buildPlayer = () => {
+    if (this.player || !this.props.stream) {
+      return;
+    }
+
+    const {id} = this.props.match.params;
+    this.player = flv.createPlayer({
+      type: 'flv',
+      url: `http://localhost:8000/live/${id}.flv`
+    });
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
+    this.player.play();
+  };
 
   render() {
     if (!this.props.stream) {
@@ -27,6 +60,19 @@ class StreamShow extends Component {
               </Header.Content>
             </Header>
           </Segment>
+        </Grid.Row>
+        <Grid.Row color="black">
+          <Grid.Column width={8}>
+            <div className="ui embed">
+              <video
+                controls
+                ref={this.videoRef}
+                style={{
+                  width: '100%'
+                }}
+              />
+            </div>
+          </Grid.Column>
         </Grid.Row>
       </Grid>
     )
